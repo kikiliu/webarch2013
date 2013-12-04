de39235cc1491368e38c2c09b407e50a4f140e7b
Kiki Liu <kikiliu@ischool.berkeley.edu>
2013-12-04 09:38:37 -0800

    Submit HW11 mrjob

--- data/combine_user_visits.py
+++ data/combine_user_visits.py
@@ -13,7 +13,7 @@ from sys import stdout
 
 
 def csv_readline(line):
-    """Given a sting CSV line, return a list of strings."""
+    """Given a string CSV line, return a list of strings."""
     for row in csv.reader([line]):
         return row
 
@@ -23,7 +23,6 @@ def main():
     the current user.  When we see a Vote/Visit line, we append the current
     user to the cell.  Then we write all lines out again (updated or not).
     """
-
     csv_writer = csv.writer(stdout)
     current_user = None
 
@@ -32,13 +31,13 @@ def main():
         if cell[0] == 'C':
             ###
             # FILL IN by replacing below:
-            pass
+            current_user = cell[2]
             # What should we update when we see a new 'C' row?
             ##/
         elif cell[0] == 'V':
             ###
             # FILL IN by replacing below:
-            pass
+            cell.append(current_user)
             # What should we update when we see a new 'V' row?
             ##/
 
